f43fd85366c8af5fabe986243f7e1b819924d128
Ahwx <jaap@jaapvd.nl>
2022-08-23 02:25:59 +0200

    Add files via upload

---
+++ Assh/MainWindow.Designer.cs
@@ -0,0 +1,64 @@
+﻿
+namespace Assh
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sshBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // sshBtn
+            // 
+            this.sshBtn.Location = new System.Drawing.Point(53, 51);
+            this.sshBtn.Name = "sshBtn";
+            this.sshBtn.Size = new System.Drawing.Size(75, 23);
+            this.sshBtn.TabIndex = 0;
+            this.sshBtn.Text = "sshBtn";
+            this.sshBtn.UseVisualStyleBackColor = true;
+            this.sshBtn.Click += new System.EventHandler(this.sshBtn_Click);
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.sshBtn);
+            this.Name = "MainWindow";
+            this.Text = "MainWindow";
+            this.Load += new System.EventHandler(this.MainWindow_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button sshBtn;
+    }
+}
+
---
+++ Assh/MainWindow.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+
+namespace Assh
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+//        string text = System.IO.File.ReadAllText(@"%userprofile%\.config\Assh\ssh_list");
+
+        private void sshBtn_Click(object sender, EventArgs e)
+        {
+            string[] lines = System.IO.File.ReadAllLines(@"ssh_list.txt");
+            MessageBox.Show("test" + lines);
+
+            System.Diagnostics.ProcessStartInfo proc = new System.Diagnostics.ProcessStartInfo();
+            proc.FileName = @"C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe";
+            proc.Arguments = "/c ssh " + lines;
+            System.Diagnostics.Process.Start(proc);
+        }
+
+        private void MainWindow_Load(object sender, EventArgs e)
+        {
+
+            if (File.Exists("ssh_list.txt")) {
+            }
+            else
+            {
+                MessageBox.Show(("It seems as if you're running Assh for the first time, please read the check the SSH file and put your info in using the wiki."));
+            }
+        }
+    }
+}
